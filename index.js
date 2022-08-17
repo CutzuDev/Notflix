956cdad7a4ee45e9499da45a899f0ba26184ca47
CutzuDev <alexfarkas05@gmail.com>
2022-08-17 14:46:09 +0300

    First Release

---
+++ index.js
@@ -0,0 +1,46 @@
+// 50f9e55e
+
+//https://api.themoviedb.org/3/movie/550?api_key=10e5b26f65216e0d907d2c1c45c6ea2c
+
+const landingBg = document.querySelector("#landing-page");
+const moviesContainerEl = document.querySelector(".movies__container");
+const defaultContainerEl = document.querySelector(".default__container");
+
+async function searchBarInput(event) {
+  const inputValue = event.target.value;
+  const movie = await fetch(
+    `http://www.omdbapi.com/?apikey=50f9e55e&s=${inputValue}`
+  );
+  const movieData = await movie.json();
+  if (!movieData.Search) {
+    moviesContainerEl.style.display = "none";
+    defaultContainerEl.style.display = "block";
+  } else if (movieData.Search) {
+    moviesContainerEl.style.display = "flex";
+    defaultContainerEl.style.display = "none";
+  }
+  moviesContainerEl.innerHTML = movieData.Search.slice(0, 9)
+    .map((movie) => moviesHTML(movie))
+    .join("");
+}
+
+function moviesHTML(movie) {
+  return `<div class="movie--container">
+  <div class="movie__contents">
+    <div class="movie__poster">
+      <img
+        src="${movie.Poster}"
+        class="movie__poster--img"
+      />
+    </div>
+    <div class="movie__info--container">
+      <div class="movie__title">${movie.Title}</div>
+      <div class="movie__year">Year: ${movie.Year}</div>
+    </div>
+  </div>
+</div>`;
+}
+
+function searchBarActive() {
+  document.querySelector(".seach__bar--ref").classList.toggle("sbActive");
+}
